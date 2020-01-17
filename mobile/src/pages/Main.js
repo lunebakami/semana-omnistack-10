53af897ba00c20088028819e2c0e67ccbe9381fa
Lune Bakami <lune.shinigami@gmail.com>
2020-01-17 11:41:57 -0300

    feat added: socketio-client

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -15,6 +15,7 @@ import {
 import { MaterialIcons } from '@expo/vector-icons';
 
 import api from '../services/api';
+import { connect, disconnect, subscribeToNewDevs } from '../services/socket';
 
 function Main({ navigation }) {
   const [devs, setDevs] = useState([]);
@@ -44,6 +45,18 @@ function Main({ navigation }) {
     loadInitalPosition();
   }, []);
 
+  useEffect(() => {
+    subscribeToNewDevs(dev => setDevs([...devs, dev]));
+  }, [devs]);
+
+  function setupWebsocket() {
+    disconnect();
+
+    const { latitude, longitude } = currentRegion;
+
+    connect(latitude, longitude, techs);
+  }
+
   async function loadDevs() {
     const { latitude, longitude } = currentRegion;
 
@@ -56,6 +69,7 @@ function Main({ navigation }) {
     });
 
     setDevs(response.data.devs);
+    setupWebsocket();
   }
 
   function handleRegionChange(region) {
